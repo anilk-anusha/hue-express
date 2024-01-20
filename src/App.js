cd3c6a8a5adfab1ea076e342b51b24a3e76110f0
Anusha Anil Kumar <anusharao.ux@gmail.com>
2024-01-19 20:08:36 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import InvalidBox from './InvalidBox'
 import SingleColor from './SingleColor'
-
 import Values from 'values.js'
+import Footer from './Footer';
 
 function App() {
   // State values
@@ -11,6 +11,7 @@ function App() {
 
   const [errorMsg, setErrorMsg] = useState("");
   const [errorShow, setErrorShow] = useState(false);
+  const [dropdown, setDropdown] = useState(10);
 
   //default value to display on the screen
   const [list, setList] = useState(new Values('#3d6aa7').all(10));
@@ -21,10 +22,12 @@ function App() {
     event.preventDefault();
     try {
       //Getting "Values" from the external library
-      let colors = new Values(color).all(10)
+      console.log("handleSubmit: dropdown: ", typeof dropdown)
+      let colors = null
+      colors = new Values(color).all(parseInt(dropdown))
       //We get an array of 20 different colors with the base color, tints and shades
       setList(colors)
-      console.log(colors)
+      console.log("handleSubmit: colors: ", colors)
     } catch (error) {
       setErrorShow(true)
       setErrorMsg("Please enter a valid HEX code")
@@ -34,12 +37,19 @@ function App() {
     }
 
   }
+
+  // Created a new function for the dropdown values - on clicking the different dropdown options - we will be dividing the initial color into tints and shades based on the dropdown number
+  const handleSelect = (event) => {
+    console.log(event.target.value)
+    setDropdown(event.target.value)
+  }
+
   return (
     <>
       <section className='container'>
         <h2>Hue Express</h2>
-        <p>Discover endless colors at your fingertips: Try Hue Express, your ultimate color generator! 🎨</p>
-        <form>
+        <p>Discover endless colors at your fingertips: Try Hue Express, your ultimate color generator! 🎨 Optionally, select color intervals from the dropdown to diversify your palette from the initial color. ⬇️</p>
+        <div>
           <input
             type="text"
             value={color}
@@ -48,10 +58,18 @@ function App() {
             // checking state value for error - if true then classname will be error
             className={`${error ? 'error' : null}`}
           />
-        </form>
+          <select className="dropdown" onChange={handleSelect} value={dropdown}>
+            <option value="1">1</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="40">40</option>
+            <option value="50">50</option>
+          </select>
+        </div>
         <button className='btn' onClick={handleSubmit}>
           Generate
-        </button> 
+        </button>
         {errorShow && <InvalidBox msg={errorMsg} />}
       </section>
       <section className='colors'>
@@ -66,7 +84,9 @@ function App() {
             />
           )
         })}
+        
       </section>
+      <Footer />
     </>
   )
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (<div className='footer'>
+    <p>Made with ❤️ from <a href='https://anusha-anilkumar.netlify.app/'>Anusha Anil Kumar</a></p>
+  </div>);
+  };
+  
+  export default Footer;
+
+
+
+
+
+
+
+
